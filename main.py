e60a0d0c7f12da9e66d12f702b8e612eeabd6764
unknown <CETECOM@AOAWSC03LC0524.duoc.acad>
2025-06-18 21:57:13 -0400

    feat/nicolas: menu agregado

---
+++ main.py
@@ -0,0 +1,41 @@
+import os
+from functions import *
+os.system("cls")
+contactos = []
+while True:
+    print("========= MENÚ =========")
+    print("1. Agregar contacto")
+    print("2. Lista de contactos")
+    print("3. Buscar contacto por nombre")
+    print("4. Eliminar un contacto")
+    print("5. Salir")
+    try:
+        opcion = int(input("ingrese opcion\n"))
+        if opcion == 1:
+            os.system("cls")
+            print("Agregar contacto")
+            agregar_contacto(contactos)
+            os.system("Pause")
+        elif opcion == 2:
+            os.system("cls")
+            print("Lista de contactos")
+            os.system("Pause")
+        elif opcion == 3:
+            os.system("cls")
+            print("Buscar contacto por nombre")
+            buscar_contacto(contactos)
+            os.system("Pause")
+        elif opcion == 4:
+            os.system("cls")
+            print("Eliminar contacto")
+            os.system("Pause")
+        elif opcion == 5:
+            os.system("cls")
+            salir_menu()
+            os.system("Pause")
+            break
+        else:
+            print("opcion ingresada no existe")
+            os.system("Pause")
+    except:
+        print("Ingrese un valor numerico correcto!")
